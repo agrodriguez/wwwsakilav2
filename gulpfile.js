ba622fbef88c05f9b96457c66fdfa5b702b80853
agrodriguez <mrboris@hotmail.com>
2016-06-17 14:49:51 -0600

    Commit diario 17-06-2016

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,20 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss', 'resources/assets/css');
+
+    mix.styles([
+    	'libs/bootstrap.min.css',    	
+    	'app.css',
+        'libs/ie10-viewport-bug-workaround.css',
+    	'libs/select2.min.css'
+    ]);
+	
+    mix.scripts([
+    	'libs/jquery.min.js',
+    	'libs/bootstrap.min.js',
+        'libs/ie10-viewport-bug-workaround.js',
+    	'libs/select2.min.js'
+    ]);
+    mix.version(['public/css/all.css','public/js/all.js']);
 });
